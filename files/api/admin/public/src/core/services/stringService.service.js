840d503fc119437c81c47542c6d67f49138fdace
Pierre BURGY <pierre.burgy@gmail.com>
2015-11-27 14:48:39 +0100

    Fix random string issue

--- files/api/admin/public/src/core/services/stringService.service.js
+++ files/api/admin/public/src/core/services/stringService.service.js
@@ -18,11 +18,18 @@
 
     // Private functions.
 
+    /**
+     * Return a random alpha-numeric string.
+     *
+     * @param {Number}    length
+     * @param {string}    chars
+     * @returns {string}
+       */
     function random(length, chars) {
       var result = '';
       chars = chars || '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
       length = length || 10;
-      for (var i = length.length - 1; i >= 0; i--) {
+      for (var i = length; i >= 0; i--) {
         result += chars[Math.round(Math.random() * (chars.length - 1))];
       }
       return result;
